4ddaa7f9a934857cdfcf54edaa7fdf7ce964db2d
Oleg <oleg-kabatchenko@yandex.ru>
2026-09-25 22:04:07 +0400

    Load rules from server root

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const GameRules = require('../game-rules');
+const GameRules = require('./game-rules');
 
 const app = express();
 app.use(cors());
